3f8f8e68de717472d7ce2d36a82135154d091bf6
Nelscrvl <96493036+Nelscrvl@users.noreply.github.com>
2026-05-06 19:29:02 +0100

    Amélioration du code dbt + ajout schéma

--- dbt/models/intermediate/DAF/int_BDD_BU.sql
+++ dbt/models/intermediate/DAF/int_BDD_BU.sql
@@ -1,7 +1,13 @@
+{# Les deux sources (BDD_BU et BDD_EDM) contiennent toutes les deux des lignes TYPE=REALISE
+   et TYPE=BUDGET pour les mêmes périmètres. Pour éviter un doublon lors du UNION ALL,
+   on met la VALEUR à 0 dans BDD_BU pour ces deux types : seule BDD_EDM fait autorité sur
+   REALISE et BUDGET. BDD_BU ne contribue que les types restants (ex: PREVISION, OBJECTIF). #}
+
 WITH UNION_TABLE AS (
 SELECT
 
 *EXCEPT(VALEUR),
+-- Anti-doublon : REALISE et BUDGET sont portés par BDD_EDM uniquement (cf. commentaire en-tête).
 CASE WHEN TYPE = "REALISE" OR TYPE = "BUDGET" THEN 0
     ELSE VALEUR
 END AS VALEUR
@@ -9,7 +15,7 @@ END AS VALEUR
 
 FROM {{ref("stg_DAF_BDD_BU_NATIVE")}}
 
-UNION ALL 
+UNION ALL
 
 SELECT
 
